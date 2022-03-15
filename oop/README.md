661246a6a82f412dc7ae5d3714405279373ea62c
Abhilash Nair <abhilashnair1994@gmail.com>
2022-03-14 21:39:06 -0400

    class methods and static methods

--- oop/README.md
+++ oop/README.md
@@ -22,19 +22,47 @@ emp_1 = Employee()
 emp_2 = Employee()
 ```
 
-## Instance variables and methods
+## Instance variables
 
 - Contain data that is unique to each instance
 
-## Class Methods
+## Class variables
 - Update data for class to collect data across instances
 
-```
+```python
 Employee.num_of_emps += 1
 ```
-```
+```python
 #Prints out instance attributes
 print(emp_1.__dict__)
 #Prints out class values and aspects of class
 print(Employee.__dict__)
+```
+
+## Class Methods
+- takes class or `cls` as first argument instead of `self`
+- use decorator `@classmethod`
+- calling class method `Employee.set_raise_amt(1.06)`
+  - can also use instance to update class method `emp_1.set_raise_amt(1.06)`
+- Use class method to create alternative constructors
+  ```python
+    @classmethod
+    def from_string(cls, emp_str):
+        first,last,pay = emp_str.split('-')
+        #creates instance
+        return cls(first,last,pay)
+  ```
+## Static Method
+  - Does not pass instance or that class but have logical connection with the class
+  - if you don't access the instance or class, it will need to be a static method
+
+```python
+    @staticmethod
+    def is_workday(day):
+        if day.weekday(5) == 5 or day.weekday() == 6:
+            return False
+        return True
+
+    print(Employee.is_workday(my_date))
+    print(new_emp_2.is_workday(my_date))
 ```
--- oop/oop.py
+++ oop/oop.py
@@ -1,5 +1,8 @@
 # Python Object-Oriented Programming
 
+import datetime
+
+
 class Employee:
     
     num_of_emps = 0
@@ -17,6 +20,21 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
     
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amount = amount
+    
+    @classmethod
+    def from_string(cls, emp_str):
+        first,last,pay = emp_str.split('-')
+        #creates instance
+        return cls(first,last,pay)
+    
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True    
     
     
 
@@ -24,26 +42,45 @@ emp_1 = Employee('Abhi', 'Nair', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
 #Prints out instance attributes
-print(emp_1.__dict__)
+# print(emp_1.__dict__)
 #Prints out class values and aspects of class
-print(Employee.__dict__)
-emp_1.raise_amount = 1.05
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
-print(Employee.raise_amount)
-print(emp_1.__dict__)
+# print(Employee.__dict__)
+# emp_1.raise_amount = 1.05
+# Employee.set_raise_amt(1.06)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+# print(Employee.raise_amount)
+# print(emp_1.__dict__)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
 
-print(emp_1.email)
-print(emp_2.email)
+# print(emp_1.email)
+# print(emp_2.email)
 
-print(emp_2.fullname())
-print(emp_1.fullname())
+# print(emp_2.fullname())
+# print(emp_1.fullname())
 
-print(Employee.num_of_emps)
+# print(Employee.num_of_emps)
 
 # self is the same as the instance of the object
 #print(Employee.fullname(emp_1))
+
+emp_str_1 = 'Abhi-Nair-700000'
+emp_str_2 = 'John-Doe-30000'
+emp_str_3 = 'Jane_Doe-90000'
+
+first,last,pay = emp_str_1.split('-')
+new_emp_1 = Employee(first,last,pay)
+# print(new_emp_1.email)
+# print(new_emp_1.pay)
+
+new_emp_2 = Employee.from_string(emp_str_2)
+# print(new_emp_2.email)
+# print(new_emp_2.pay)
+my_date = datetime.date(2022,3,14)
+print(Employee.is_workday(my_date))
+print(new_emp_2.is_workday(my_date))
+
+
